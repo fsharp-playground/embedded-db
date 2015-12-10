2c8c4037e382192d8755fcf30f3e4f80528ce623
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-12-11 01:18:45 +0700

    test reven db

---
+++ CouchbaseLite.Tests/CouchbaseSpec.fs
@@ -0,0 +1,57 @@
+﻿module CouchbaseSpec 
+
+open FsUnit
+open NUnit.Framework
+open Couchbase.Lite
+open System
+open System.Linq
+
+type Property = {
+    Name: string
+    Value: obj
+}
+
+type Info = {
+   Path: string
+   FielName: string
+   Properties: Property list
+}
+
+module Db = 
+    let private name = "test"
+    let private manager = Manager.SharedInstance
+    let test = manager.GetDatabase(name)
+
+[<Test>]
+let ShouldQueryDocument() =
+    let view = Db.test.GetView("info")
+    let query = view.CreateQuery()
+    ()
+
+[<Test>]
+let ShouldRetreiveDocument() =
+    let doc = Db.test.GetDocument("___")
+    doc.Properties |> should equal null
+
+[<Test>]
+let ShouldCreateEmptyDatabaseFile() =
+    let doc = Db.test.CreateDocument()
+
+    let dict = new System.Collections.Generic.Dictionary<String, Object>()
+    dict.Add("name", "wk")
+    dict.Add("age", 20)
+
+    let info = {
+        Path = "/root"
+        FielName = "Hello.txt"
+        Properties = [
+                        { Name = "A"; Value = 100 }
+                        { Name = "B"; Value = 100 }
+                        { Name = "C"; Value = 100 } ]
+    }
+
+    dict.Add("info", info)
+
+    let revision = doc.PutProperties dict
+
+    revision.Database.Name  |> should equal "test"
---
+++ CouchbaseLite.Tests/RevenDbSpec.fs
@@ -0,0 +1,54 @@
+﻿module RevenDbSpec
+
+open Raven.Client.Embedded
+open FsUnit
+open NUnit.Framework
+open System.Linq
+
+type Property = {
+    Name: string
+    Value: obj
+}
+
+type Info = {
+    Name: string
+    Path: string
+    Properties: Property list
+}
+
+let db() = 
+    let db = "test"
+    let store = new EmbeddableDocumentStore()
+    store.DataDirectory <- "Database"
+    store.DefaultDatabase <- db
+    store.Initialize()
+
+[<Test>]
+let ShouldQueryDocument() =
+    let store = db()
+    let session = store.OpenSession()
+    let doc = session.Load<Info>("10")
+    doc |> should equal null
+
+    let docs = session.Query<Info>().Where( fun x -> x.Path = "/root")
+
+    docs.Count() |> should equal 1
+    docs.FirstOrDefault().Name |> should equal "Test.pdf"
+   
+
+[<Test>]
+let ShouldCreateEmptyDatabase() =
+    let store = db()
+    let info = {
+        Info.Name = "Test.pdf"
+        Path = "/root"
+        Properties = [
+                        { Name = "a"; Value = 1}
+                        { Name = "b"; Value = 2} ]
+    }
+
+    let session = store.OpenSession()
+    session.Store(info)
+    session.SaveChanges()
+
+
